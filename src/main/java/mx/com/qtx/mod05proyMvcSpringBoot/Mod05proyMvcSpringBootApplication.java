117650371b45467c0a90cf05ba29c2122ac927d2
alejandroQtx <qualitrain@gmail.com>
2026-03-06 16:20:38 -0600

    primer commit

---
+++ src/main/java/mx/com/qtx/mod05proyMvcSpringBoot/Mod05proyMvcSpringBootApplication.java
@@ -0,0 +1,13 @@
+package mx.com.qtx.mod05proyMvcSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mod05proyMvcSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mod05proyMvcSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mx/com/qtx/mod05proyMvcSpringBoot/Mod05proyMvcSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.com.qtx.mod05proyMvcSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mod05proyMvcSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
