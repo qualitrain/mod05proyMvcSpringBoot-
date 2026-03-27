0ae6c2e33d45cdcf53f2091be24c5663d21f52e3
alejandroQtx <qualitrain@gmail.com>
2026-03-27 17:25:22 -0600

    Ejm super simple de Thymeleaf y uso del objeto Model

--- src/main/java/mx/com/qtx/mod05proyMvcSpringBoot/web/VentasController.java
+++ src/main/java/mx/com/qtx/mod05proyMvcSpringBoot/web/VentasController.java
@@ -1,9 +1,12 @@
 package mx.com.qtx.mod05proyMvcSpringBoot.web;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.math.BigDecimal;
 
@@ -12,14 +15,19 @@ public class VentasController {
     final private static Logger log = LoggerFactory.getLogger(VentasController.class);
 
     @GetMapping("/consultarArticulo")
-    public String irAconsulaArticulo(){
+    public String irAconsulaArticulo(HttpServletRequest req){
         log.info("irAconsulaArticulo()");
         return "consultaArticulo";
     }
 
     @GetMapping("/buscarArticulos")
-    public String buscarArticulo(String cveArticulo, String descripcion, BigDecimal costoProv1, BigDecimal precioLista){
+    public String buscarArticulo(@RequestParam(name = "precioLista",defaultValue = "1.0") BigDecimal precioLista,
+                                 String cveArticulo, String descripcion,
+                                 @RequestParam(defaultValue = "0.0") BigDecimal costoProv1, Model model){
         log.info("buscarArticulo({},{},{},{})", cveArticulo, descripcion, costoProv1, precioLista);
+
+        model.addAttribute("mensaje","Metodo no disponible. Lo siento mucho");
+
         return "consultaArticulo";
     }
 }
