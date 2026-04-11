04b0796d91c547dcbcc07df0b7b3219d16c5f6ae
alejandroQtx <qualitrain@gmail.com>
2026-04-10 19:09:03 -0600

    Ejm de messageSource y de i18n ok

--- src/main/java/mx/com/qtx/mod05proyMvcSpringBoot/Mod05proyMvcSpringBootApplication.java
+++ src/main/java/mx/com/qtx/mod05proyMvcSpringBoot/Mod05proyMvcSpringBootApplication.java
@@ -2,6 +2,11 @@ package mx.com.qtx.mod05proyMvcSpringBoot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.FixedLocaleResolver;
+
+import java.util.Locale;
 
 @SpringBootApplication
 public class Mod05proyMvcSpringBootApplication {
@@ -9,5 +14,13 @@ public class Mod05proyMvcSpringBootApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(Mod05proyMvcSpringBootApplication.class, args);
 	}
+
+    @Bean
+    public LocaleResolver localeResolver() {
+    //    FixedLocaleResolver resolver = new FixedLocaleResolver(Locale.of("es","MX"));
+        FixedLocaleResolver resolver = new FixedLocaleResolver(Locale.US);
+    //    FixedLocaleResolver resolver = new FixedLocaleResolver(Locale.of("or","IN"));
+        return resolver;
+    }
 
 }
