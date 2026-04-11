c87d6d5f87fc81fd48267c387581106dc8a12ccd
alejandroQtx <qualitrain@gmail.com>
2026-04-10 19:46:21 -0600

    Ejm de messageSource y de i18n con uso desde el controller ok

--- src/main/java/mx/com/qtx/mod05proyMvcSpringBoot/web/VentasController.java
+++ src/main/java/mx/com/qtx/mod05proyMvcSpringBoot/web/VentasController.java
@@ -6,17 +6,24 @@ import mx.com.qtx.mod05proyMvcSpringBoot.servicios.IGestorVentas;
 import org.jspecify.annotations.Nullable;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.math.BigDecimal;
+import java.util.Locale;
 
 @Controller
 public class VentasController {
     final private static Logger log = LoggerFactory.getLogger(VentasController.class);
     private final IGestorVentas gestorVtas;
+
+    @Autowired
+    MessageSource ms;
 
     public VentasController(IGestorVentas gestorVtas) {
         this.gestorVtas = gestorVtas;
@@ -31,25 +38,33 @@ public class VentasController {
     @GetMapping("/buscarArticulos")
     public String buscarArticulo(String cveArticulo, Model model){
 
+        final String msgArtNoEspec = getMensajeI18n("servicio.articulos.error.artNoEspecificado");
+        final String msgArtNoExiste = getMensajeI18n("servicio.articulos.error.artNoExiste");
         if(cveArticulo == null){
-            model.addAttribute("mensaje","Articulo no especificado");
+            model.addAttribute("mensaje",msgArtNoEspec);
             model.addAttribute("articulo", getArticuloVacio());
             return "consultaArticulo";
         }
         if(cveArticulo.trim().isEmpty()){
-            model.addAttribute("mensaje","Articulo no especificado");
+            model.addAttribute("mensaje",msgArtNoEspec);
             return "consultaArticulo";
         }
 
         Articulo art = this.gestorVtas.getArticuloXID(cveArticulo);
         if(art == null){
-            model.addAttribute("mensaje","Articulo no encontrado");
+            model.addAttribute("mensaje",msgArtNoExiste);
             return "consultaArticulo";
         }
 
         model.addAttribute("articulo",art);
         log.info(art.toString());
         return "consultaArticulo";
+    }
+
+    private String getMensajeI18n(String llaveMsg) {
+        Locale localidadMsgs = LocaleContextHolder.getLocale();
+        String msgArtNoEspec = ms.getMessage(llaveMsg,null,localidadMsgs);
+        return msgArtNoEspec;
     }
 
     private Articulo  getArticuloVacio() {
